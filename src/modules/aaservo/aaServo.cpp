0c782ce886062c3f9acee8ab3be711215fcd93de
Ahmad <aziad1998@gmail.com>
2025-01-09 20:06:12 -0500

    module for later testing

---
+++ src/modules/aaservo/aaServo.cpp
@@ -0,0 +1,9 @@
+#include <px4_platform_common/log.h>
+
+extern "C" __EXPORT int aaservo_main(int argc, char *argv[]);
+
+int aaservo_main(int argc, char *argv[])
+{
+	PX4_INFO("Hello WOWOWOW!");
+	return OK;
+}
